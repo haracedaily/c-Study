8a777c2e44480f3cf3b4c5ba23d939ee837f2720
JH\nj700 <nj7001@naver.com>
2025-07-08 00:16:33 +0900

    no message

--- TODO-List/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ TODO-List/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b856bfcb9db55866f82a1c973b17b730c37a4ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2060f4e6ad561519cc3cf818145e68a774d318dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
